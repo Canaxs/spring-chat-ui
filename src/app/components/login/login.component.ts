f9f65d9a7a33ae8970579e449cf7060eebddeeed
Meriç Cana <mmericcana@gmail.com>
2024-02-02 00:50:05 +0300

    login ts completed
    login ts completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AuthService} from "../../service/auth.service";
+import {Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
+import {Observable} from "rxjs";
+import {Token} from "../../models/token";
 
 @Component({
   selector: 'app-login',
@@ -8,9 +13,11 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   rightPanel= false;
+  @Input() loginUsername: string | undefined;
+  @Input() loginPassword: string | undefined;
 
 
-  constructor() {
+  constructor(private AuthService: AuthService,private router: Router) {
   }
 
   ngOnInit(): void {
@@ -19,10 +26,26 @@ export class LoginComponent implements OnInit {
     this.rightPanel = !this.rightPanel;
   }
   login() {
+    const username = document.getElementById("loginUsername");
+    const password = document.getElementById("loginPassword");
+    let token: Observable<Token>;
+    if(username.innerText != null && password.innerText != null) {
+      token = this.AuthService.login(username.innerText,password.innerText);
+    }
+    token.subscribe(response => {
+      if(response.token != null) {
+        this.router.navigate(['/chat']);
+      }
+    }, err => {
+    })
 
   }
   register() {
+    const username = document.getElementById("registerUsername");
+    const password = document.getElementById("registerPassword");
+    if(username.innerText != null && password.innerText != null) {
 
+    }
   }
 
 }
